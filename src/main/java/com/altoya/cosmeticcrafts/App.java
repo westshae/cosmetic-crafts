abb8cc1b36262bde8fba5d640712db6b3bbf361b
westshae <shaewest02@gmail.com>
2021-12-06 12:52:12 +1300

    Added custom item that gives specific model, based on customModelData

--- src/main/java/com/altoya/cosmeticcrafts/App.java
+++ src/main/java/com/altoya/cosmeticcrafts/App.java
@@ -1,12 +1,37 @@
 package com.altoya.cosmeticcrafts;
+import javax.xml.stream.events.Namespace;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 public class App extends JavaPlugin {
     @Override
     public void onEnable() {
-        getLogger().info("Hello, SpigotMC!");
+        this.getServer().getPluginManager().registerEvents(new JoinEvent(), this);
+        Bukkit.addRecipe(getRecipe());
     }
     @Override
     public void onDisable() {
-        getLogger().info("See you again, SpigotMC!");
+    }
+
+    private ShapedRecipe getRecipe(){
+        ItemStack item = new ItemStack(Material.GOLDEN_SWORD);
+        ItemMeta meta = item.getItemMeta();
+        meta.setCustomModelData(1234567);
+
+        item.setItemMeta(meta);
+
+        NamespacedKey key = new NamespacedKey(this, "custom_sword");
+        ShapedRecipe recipe = new ShapedRecipe(key, item);
+
+        recipe.shape(" B ", " B ", " R ");
+        recipe.setIngredient('B', Material.GOLD_BLOCK);
+        recipe.setIngredient('R', Material.BLAZE_ROD);
+
+        return recipe;
     }
 }
