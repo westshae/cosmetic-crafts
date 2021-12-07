543a491c4e7a7d2384d026005a039fd1104b5a82
westshae <shaewest02@gmail.com>
2021-12-07 21:03:40 +1300

    Cleaned GiveItem.java

--- src/main/java/com/altoya/cosmeticcrafts/createItem/GiveItem.java
+++ src/main/java/com/altoya/cosmeticcrafts/createItem/GiveItem.java
@@ -5,7 +5,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.HumanEntity;
 import org.bukkit.inventory.ItemStack;
 
 public class GiveItem implements CommandExecutor {
@@ -14,21 +13,15 @@ public class GiveItem implements CommandExecutor {
     if (command.getName().equalsIgnoreCase("giveskin")) {
       if(args.length != 1) return true;
       if (sender.hasPermission("cosmeticcrafts.giveskin")) {
-        try{
         FileConfiguration config = Bukkit.getServer().getPluginManager().getPlugin("cosmeticcrafts").getConfig();
-        String itemExist = config.getString("items." + args[0].toString());
-        if(itemExist == null){
-          sender.sendMessage("REEEEEE");
+
+        if(config.getString("items." + args[0].toString()) == null){
+          sender.sendMessage("This custom model doesn't exist!");
           return true;
         }
 
         ItemStack item = new Item().getModelChanger(config, args[0]);
-
-        HumanEntity player = Bukkit.getPlayerExact(sender.getName());
-        player.getInventory().addItem(item);
-        }catch(Exception e){
-          System.out.println(e);
-        }
+        Bukkit.getPlayerExact(sender.getName()).getInventory().addItem(item);
       }
     }
     return true;
