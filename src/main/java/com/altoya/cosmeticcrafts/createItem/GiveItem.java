2e7e0b4419d963374e24c1062999f1c0f06da83f
westshae <shaewest02@gmail.com>
2021-12-07 21:28:52 +1300

    Added ability to /giveskin [ID] [PLAYERNAME], where playername is optional

--- src/main/java/com/altoya/cosmeticcrafts/createItem/GiveItem.java
+++ src/main/java/com/altoya/cosmeticcrafts/createItem/GiveItem.java
@@ -11,20 +11,37 @@ public class GiveItem implements CommandExecutor {
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if (command.getName().equalsIgnoreCase("giveskin")) {
-      if(args.length != 1) return true;//Must be a args length of 1
       if (sender.hasPermission("cosmeticcrafts.giveskin")) {
-        //Loads config.yml
-        FileConfiguration config = Bukkit.getServer().getPluginManager().getPlugin("cosmeticcrafts").getConfig();
+        if (args.length == 1) {
+          // Loads config.yml
+          FileConfiguration config = Bukkit.getServer().getPluginManager().getPlugin("cosmeticcrafts").getConfig();
 
-        //If the custom item hasn't been found. return;
-        if(config.getString("items." + args[0].toString()) == null){
-          sender.sendMessage("This custom model doesn't exist!");
+          // If the custom item hasn't been found. return;
+          if (config.getString("items." + args[0].toString()) == null) {
+            sender.sendMessage("This custom model doesn't exist!");
+            return true;
+          }
+
+          // Creates modelChanger item, gives to player
+          ItemStack item = new Item().getModelChanger(config, args[0]);
+          Bukkit.getPlayerExact(sender.getName()).getInventory().addItem(item);
+        } else if (args.length == 2) {
+          // Loads config.yml
+          FileConfiguration config = Bukkit.getServer().getPluginManager().getPlugin("cosmeticcrafts").getConfig();
+
+          // If the custom item hasn't been found. return;
+          if (config.getString("items." + args[0].toString()) == null) {
+            sender.sendMessage("This custom model doesn't exist!");
+            return true;
+          }
+
+          // Creates modelChanger item, gives to player
+          ItemStack item = new Item().getModelChanger(config, args[0]);
+          if(Bukkit.getPlayerExact(args[1]) == null) return true;
+          Bukkit.getPlayerExact(args[1]).getInventory().addItem(item);
+        } else {
           return true;
         }
-
-        //Creates modelChanger item, gives to player
-        ItemStack item = new Item().getModelChanger(config, args[0]);
-        Bukkit.getPlayerExact(sender.getName()).getInventory().addItem(item);
       }
     }
     return true;
