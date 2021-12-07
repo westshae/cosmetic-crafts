696fd0b40de09e1b91c4f2e41728a76417bc2a6e
westshae <shaewest02@gmail.com>
2021-12-08 11:21:18 +1300

    small update to remove testing code

--- src/main/java/com/altoya/cosmeticcrafts/App.java
+++ src/main/java/com/altoya/cosmeticcrafts/App.java
@@ -13,14 +13,12 @@ public class App extends JavaPlugin {
 
         //Sets server commands/listener events
         this.getCommand("giveskin").setExecutor(new GiveItem());
-        this.getServer().getPluginManager().registerEvents(new JoinEvent(), this);
         this.getServer().getPluginManager().registerEvents(new ChangeModel(), this);
     }
 
     public void loadConfig() {
         File file = new File(getDataFolder(), "item.yml");//Gets config
         if(!file.exists()){//If config file doesn't exist, create new file
-            getConfig().addDefault("texturepack.link", "https://www.dropbox.com/sh/f3ie51vsn6pi1wj/AAAsq6jQvAp3e-yF6DHmFF5Ja?dl=1");
             getConfig().addDefault("items.0.modelID", "1234567");
             getConfig().addDefault("items.0.name", "&6&lSkinChanger: &b&lCosmeticChanger");
             getConfig().addDefault("items.0.lore", Arrays.asList("&bChanges your item models", "", "&7Use on swords"));
